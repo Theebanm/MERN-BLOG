149db37ddbb8554354ca1e8c1a4bea3051b5bded
Theebanm <parthiparthiban7750@gmail.com>
2024-03-24 13:59:57 +0530

    user model created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log(`MongoDB connected `))
+  .catch((error) => console.log(error));
 
 app.listen(8393, () => {
   console.log(`Server is Running On 8393`);
