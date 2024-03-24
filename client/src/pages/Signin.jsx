bbef7e16def394a566b344d4a556a0f8c52414ca
Theebanm <parthiparthiban7750@gmail.com>
2024-03-24 22:03:23 +0530

    complete sign in functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,117 @@
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Signin = () => {
-  return <div>Sign in</div>;
+const SignIn = () => {
+  const [formData, setFormData] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (response.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-10">
+        {/* left side */}
+        <div className="flex-1">
+          <Link
+            to="/"
+            className=" text-4xl sm:text-xl font-semibold dark:text-white "
+          >
+            <span className="px-2 py-1 bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">
+              THEEBAN'S
+            </span>
+            BLOG
+          </Link>
+          <p className="text-sm mt-5 ">
+            "Theeban Blog is a captivating online platform where I share my
+            thoughts, experiences, and insights on various topics. Join me on
+            this journey of exploration and discovery, where every post is
+            crafted to inform, entertain, and ignite meaningful conversations."
+          </p>
+        </div>
+
+        {/* right side */}
+        <div className="flex-1">
+          <div className="p-5 mb-4 bg-gradient-to-r from-blue-600 to-pink-600 text-3xl text-white ">
+            Sign in
+          </div>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            {errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )}
+
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3"> Loading...</span>
+                </>
+              ) : (
+                "SignUp"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account ?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Signin;
+export default SignIn;
